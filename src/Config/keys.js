b6deeb921747bc06cb1bfa0a468aa01d5a0fd0f9
VaheMeekz <vahe.mkrtchyan.a3@gmail.com>
2022-06-14 18:52:35 +0400

    config

--- src/Config/keys.js
+++ src/Config/keys.js
@@ -1,4 +1,4 @@
-export const backUrl = "http://localhost:7000";
+export const backUrl = "https://jewelry001.herokuapp.com";
 export const language = localStorage.getItem("lang")
 export const makeArray = (length) => {
     return new Array(length).fill(undefined).map((_, index) => index);
